c4e055c4610e3a4986f42ad7f0689a76931fe5c2
Rajesh <rajeshceg3@gmail.com>
2024-08-16 05:58:11 +0000

    Connect the socket to Server Endpoint

--- main.cpp
+++ main.cpp
@@ -4,6 +4,8 @@
 int main() {
   boost::asio::io_context io_context;
   boost::asio::ip::tcp::socket socket(io_context); 
+  boost::asio::ip::tcp::endpoint endpoint(boost::asio::ip::address::from_string("127.0.0.1"),8080); 
+    socket.connect(endpoint);
   std::cout << "Hello, Asio world!" << std::endl;
   return 0;
 }
