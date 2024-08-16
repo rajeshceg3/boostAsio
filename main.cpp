651b12bc11d20a1fc6d878f4f039f4c0d7660e30
Rajesh <rajeshceg3@gmail.com>
2024-08-16 05:53:11 +0000

    Create io context

--- main.cpp
+++ main.cpp
@@ -1,7 +1,8 @@
-#include <iostream>
 #include <boost/asio.hpp>
+#include <iostream>
 
 int main() {
+  boost::asio::io_context io_context;
   std::cout << "Hello, Asio world!" << std::endl;
   return 0;
 }
