12026d0d3135584c656d59b0438e05b643566b8d
Rajesh <rajeshceg3@gmail.com>
2024-08-16 06:03:00 +0000

    Send message to socket

--- .ccls-cache/@home@runner@boostAsio/main.cpp
+++ .ccls-cache/@home@runner@boostAsio/main.cpp
@@ -4,8 +4,13 @@
 int main() {
   boost::asio::io_context io_context;
   boost::asio::ip::tcp::socket socket(io_context); 
-  boost::asio::ip::tcp::endpoint endpoint(boost::asio::ip::address::from_string("127.0.0.1"),8080); 
-    socket.connect(endpoint);
   std::cout << "Hello, Asio world!" << std::endl;
+  
+  boost::asio::ip::tcp::endpoint endpoint(boost::asio::ip::address::from_string("127.0.0.1"),8080); 
+  socket.connect(endpoint);
+  
+  std::string message = "Hello from client!";
+  boost::asio::write(socket, boost::asio::buffer(message));
+
   return 0;
 }
